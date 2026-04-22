50545bcfff897100232df52e9e5e5f6f384dcfa3
tAnYa1109leekhi <143273304+tAnYa1109leekhi@users.noreply.github.com>
2026-04-22 16:33:52 +0530

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 INSTALLED_APPS = [
     'django.contrib.admin',   
     'django.contrib.auth',
@@ -10,8 +11,32 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders'
 ]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware', 
+    'corsheaders.middleware.CorsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',  
+        'DIRS': [BASE_DIR / 'mynotes/build'],
+        'APP_DIRS': True, 
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 ROOT_URLCONF = 'notesapp.urls'
-BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 DATABASES = {
