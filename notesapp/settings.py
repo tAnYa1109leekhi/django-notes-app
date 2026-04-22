58361ff91755618636b5ca19c4c6fd007153c4c3
tAnYa1109leekhi <143273304+tAnYa1109leekhi@users.noreply.github.com>
2026-04-22 16:19:04 +0530

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -1,4 +1,15 @@
 from pathlib import Path
+INSTALLED_APPS = [
+    'django.contrib.admin',   
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'rest_framework',
+    'corsheaders'
+]
 ROOT_URLCONF = 'notesapp.urls'
 BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
