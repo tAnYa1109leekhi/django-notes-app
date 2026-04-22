388f6e3c641125e5b0bfba8861cf9a2bc0cf9ad0
tAnYa1109leekhi <143273304+tAnYa1109leekhi@users.noreply.github.com>
2026-04-22 15:54:54 +0530

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -1,6 +1,10 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
